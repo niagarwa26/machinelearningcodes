56cbaaf6a1368b00d5933a0d0fd9a15af1e196b5
niagarwa26 <39934667+niagarwa26@users.noreply.github.com>
2018-06-21 20:44:48 +0530

    Add files via upload
    this includes  code for functions related to matplotlib  of python

---
+++ matplotlib_code.py
@@ -0,0 +1,21 @@
+#!usr/bin/python3
+
+import matplotlib as plt
+import os
+
+x=[2,3]
+y=[4,9]
+
+x1=[8,3]
+y1=[7,2]
+
+
+plt.X label ('time')
+plt.Y label ('speed')
+
+
+plt.plot(x,y)
+plt.plot(x1,y1)
+
+
+plt.show()
